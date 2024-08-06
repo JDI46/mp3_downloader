b0ad65b2925fe6f2365f10341abc685bd6263380
Xander Whitby <xwhitby20@gmail.com>
2024-08-05 21:01:12 -0600

    instantiated download and info classes

---
+++ main.py
@@ -0,0 +1,27 @@
+from pytube import YouTube
+
+class Download:
+    def __init__(self, download_video):
+        self.download_video = download_video
+
+
+    def get_video(self):
+        pass
+
+
+class Info:
+    def __init__(self, progressive_stream, dash_stream, audio_stream, mp4_stream):
+        self.progressive_stream = progressive_stream
+        self.dash_stream = dash_stream
+        self.audio_stream = audio_stream
+        self.mp4_stream = mp4_stream
+        
+
+class Play:
+    pass
+
+
+class Storage:
+    pass
+
+
